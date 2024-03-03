593bb50ca9143121e24b07ffc70d37236df5656d
Walter Daniel <walterdcarrizo.19@gmail.com>
2024-03-03 20:48:06 -0300

    Se instaló tanstack query para el manejo de estado asincrono

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,24 +1,20 @@
-import { useEffect, useState } from "react";
-import { products } from '../../api/products';
-import { Product } from "./products.interfaces";
-import { Grid } from "@mui/material";
+// import { useEffect, useState } from "react";
+
 import { CardComponent } from "../../components";
 import { CardGrid } from "../../components/CardSkeleton";
 import HeroComponent from "../../components/hero";
 
+import { Grid } from "@mui/material";
+import { useProducts } from "../../hooks/useProducts";
 
 
 export const HomePage = () => {
 
-  const [cardProducts, setCardProducts ] = useState<Product[] | null>(null)
+  const productsQuery = useProducts();
 
-  useEffect(() => {
-    products.getAll().then((r) => {
-      setCardProducts(r.data.allProducts);
-    }).catch((e) =>{
-      console.log(e)
-    });
-  },[])
+  if(productsQuery.isLoading){
+    return (<CardGrid />)
+  }
   
   return (
 
@@ -27,19 +23,19 @@ export const HomePage = () => {
     <HeroComponent />
     
       <div style={{ marginTop: '4rem' }}>
-          {
-            cardProducts !== null? (
+          
+            
               <Grid container spacing={2} direction="row" key="hola">
                 {
-                  cardProducts!.map((item) =>(
+                  productsQuery.data?.map((item) =>(
                     <Grid item xs={3} key={item._id}>
                       <CardComponent name={item.name} price={item.price} detail={item.detail} id={item._id}/>
                     </Grid>
                   ))  
                 }
               </Grid>
-            ): <CardGrid />
-          }
+            
+          
         </div>
     </>
 
